54fac13a8986cffdf6eee2f46955604a91e59419
LaraMi92 <67866093+LaraMi92@users.noreply.github.com>
2020-11-29 10:48:22 +0100

    first commit after edition

---
+++ app.js
@@ -0,0 +1,31 @@
+
+//selecting in the DOM
+let main = document.querySelector(".main");
+let grid = document.querySelector(".grid");
+main.appendChild(grid);
+
+function multiply (number) {
+    //generate number of squares in grid
+    for(let i=0; i < (number*number); i++){
+
+        let square = document.createElement("span");
+        square.classList.add("square");
+        grid.appendChild(square);
+    
+    //click event to apply color
+    function clickColor(event) {
+        event.currentTarget.classList.add("aqua");
+
+        if(event.currentTarget.classList.contains("aqua")) {
+            event.currentTarget.classList.toggle("salmon");
+        }
+        
+        
+
+    };
+
+    square.addEventListener('click', clickColor);
+};
+}
+
+multiply(65);
